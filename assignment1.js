50df853053e77ebc4e7bb9f92bcc0530532d9739
Fox <jcfox@labs.neit.edu>
2018-01-25 12:12:14 -0500

    fixed the links to basic html

--- assignment1.js
+++ assignment1.js
@@ -6,16 +6,16 @@ app.use(express.static("public"))
 app.use(express.static("src/views"))
 
 //static routes
-app.get("/", function(req, res){
-	res.send(index.html);
+app.get("/", function(req, response){
+	response.render("index.html");//changed res.send to response.render
 })
 
-app.get("/1", function(req, res){
-	res.send(addToList.html);
+app.get("/", function(req, response){
+	response.render("addToList.html");
 })
 
-app.get("/2", function(req, res){
-	res.send(logIn.html);
+app.get("/", function(req, response){
+	response.render("logIn.html");
 })
 
 //Listen on port
